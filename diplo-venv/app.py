76610f1642a75e6e55d2583324bb2e5ba0fa26ca
Palatio <humberto1nacho@gmail.com>
2025-01-12 21:03:36 -0600

    agregando forms para manejo de informacion

--- diplo-venv/app.py
+++ diplo-venv/app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        #username = request.form.get('username')
+        #password = request.form.get('password')
+        print("Hi")
+        return "success"
 
 
 if (__name__ == "__main__"):
-    app.run(debug = True)
+    app.run()
